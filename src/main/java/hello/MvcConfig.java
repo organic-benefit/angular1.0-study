7aa79ad87ce6944a753afbe695f9a1ecb9e677db
doublemetal <dongchul.kim@nhn.com>
2016-10-05 16:34:09 +0900

    Add study-1

---
+++ src/main/java/hello/MvcConfig.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("hello");
+		registry.addViewController("/home").setViewName("hello");
+		registry.addViewController("/index").setViewName("hello");
+		registry.addViewController("/hello").setViewName("hello");
+	}
+
+}
